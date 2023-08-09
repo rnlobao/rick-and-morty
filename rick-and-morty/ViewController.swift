6db3ed2ce93ea4d3465a10f2f3e4f72d2cca3d53
Robson Novato <rnlobao42@gmail.com>
2023-08-09 13:17:27 -0300

    Initial Commit

---
+++ rick-and-morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rick-and-morty
+//
+//  Created by Robson Novato on 09/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
